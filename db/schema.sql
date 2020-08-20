bc7a542d1ad4f8e0a0ead22125e16f0871f21651
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-08-20 10:55:27 -0400

    created schema for department, role, and employee tables

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS organization_db;
+CREATE DATABASE organization_db;
+USE organization_db;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(8,2) NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT DEFAULT null,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role(id)
+);
